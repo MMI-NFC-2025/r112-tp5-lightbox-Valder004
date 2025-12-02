ae71d812b5e600901d323a2a9ea05553cec20dc0
Valder004 <valentin.deroo@edu.univ-fcomte.fr>
2025-12-02 17:00:47 +0100

    amélioration CSS

--- src/js/script.js
+++ src/js/script.js
@@ -19,3 +19,27 @@ lightBox.showModal();
 
 lightBox.close();
 
+const updateButton = document.getElementById("lightbox");
+const cancelButton = document.getElementById("lightbox-cancel");
+const dialog = document.getElementById("lightbox-dialog");
+dialog.returnValue = "lightboxClosed";
+
+function openCheck(dialog) {
+  if (dialog.open) {
+    console.log("Dialog opened");
+  } else {
+    console.log("Dialog closed");
+  }
+}
+
+// Update button opens a modal dialog
+updateButton.addEventListener("click", () => {
+  dialog.showModal();
+  openCheck(dialog);
+});
+
+// Form cancel button closes the dialog box
+cancelButton.addEventListener("click", () => {
+  dialog.close("animalNotChosen");
+  openCheck(dialog);
+});
