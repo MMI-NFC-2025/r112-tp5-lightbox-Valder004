9d358c804b2cfff3f5885b76ca0f6c7a7913d9eb
Valder004 <valentin.deroo@edu.univ-fcomte.fr>
2025-12-02 16:30:27 +0100

    ouverture du dialogue

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,21 @@
 // *** LIGHTBOX ***
+
+
+
+const lightBox = document.querySelector("#lightbox");
+
+const lightBoxImage = document.querySelector("#lightbox img");
+
+lightBoxImage?.addEventListener("click", () => lightBox.close());
+document.body.addEventListener("click", (evt) => {
+  if (!evt.target.matches("[data-full-img]")) return;
+  lightBoxImage.src = evt.target.dataset.fullImg;
+  lightBox.showModal();
+});
+
+lightBox = document.querySelector("#lightbox");
+
+lightBox.showModal();
+
+lightBox.close();
+
